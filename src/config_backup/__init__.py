b984e50b49fc8b1dfa7f969b8c2e5fdf3aebe50b
hanabi-bro <hanabi.bro@gmail.com>
2026-09-15 14:27:07 +0900

    Initial scaffold for config_backup

---
+++ src/config_backup/__init__.py
@@ -0,0 +1,3 @@
+"""config_backup package."""
+
+__version__ = "0.1.0"
---
+++ src/config_backup/cli.py
@@ -0,0 +1,9 @@
+"""config_backup CLI エントリポイント."""
+
+
+def main() -> None:
+    print("config_backup: hello from config_backup")
+
+
+if __name__ == "__main__":
+    main()
